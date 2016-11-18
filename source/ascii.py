23babab4bf6d1b6ffa02011f500591d3a24c3538
Einscrew <manel.rodrigo.reis@gmail.com>
2016-11-18 18:35:48 +0000

    Missing error handling

--- source/ascii.py
+++ source/ascii.py
@@ -2,7 +2,7 @@
 
 import sys #doing nothing right now
 
-def ascii(txt):  #073 068 126
+def text(txt):  #073 068 126
     i = 0
     c = 0
     buffer = ''
@@ -15,6 +15,30 @@ def ascii(txt):  #073 068 126
         output = output + chr(int(buffer))
         buffer = ''
         i+=4
-    print output
+    return output
 
-ascii(str(raw_input('Insira em ASCII:\n')))
+def ascii(txt): #blahblahblah
+    i = 0
+    output = ''
+    while i < len(txt):
+        output = output + str(getAscii(txt[i]))
+        i+=1
+    return output
+
+def getAscii(character):
+    if len(str(ord(character))) < 3:
+        return '0' + str(ord(character)) + ' '
+    else:
+        return str(ord(character)) + ' '
+
+def main():
+    answer = input('Press:\t[other to quit]\n1-to Ascii\t2-to Text\n')
+    while(answer == 1 or answer == 2):
+        if answer == 1:
+            print ascii(str(raw_input('Insert the text:\n')))
+        if answer == 2:
+            print text(str(raw_input('Insert ASCII code:\n')))
+        answer = input('\n\nPress:\n1-to Ascii\t2-to Text\n')
+    return 0
+
+main()
